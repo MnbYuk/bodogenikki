70ab2a36b9fb95a988db91ea12770384a21393e6
MnbYuk <haiiroorennji@gmail.com>
2024-06-05 07:25:05 +0000

    public views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
+  get 'comments/new'
+  get 'comments/edit'
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'users/mypage'
+  get 'users/edit'
+  get 'users/show'
   devise_for :users
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
