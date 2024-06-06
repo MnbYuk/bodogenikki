c534dfd9c032e56da606664843bc991f846f5427
MnbYuk <haiiroorennji@gmail.com>
2024-06-06 13:22:02 +0000

    フォルダわけ

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/edit'
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'users/mypage'
-  get 'users/edit'
-  get 'users/show'
+
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
 
+  resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+  resources :comments, only: [:new, :edit, :create, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
+
+  get 'users/mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  end
